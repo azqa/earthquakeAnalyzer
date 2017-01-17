53d48b7587a97e9934c0d2476b1f02a5c5f3415d
Azqa Nadeem <Azqa Nadeem>
2017-01-17 02:58:06 +0100

    restructured code

---
+++ assets/data/markercolor.js
@@ -0,0 +1,707 @@
+var colordata= [
+    {
+      "Country": "HAITI",
+      "val": 272409.5367,
+      "colr": "dark"
+    },
+    {
+      "Country": "CHINA",
+      "val": 12434.12335,
+      "colr": "dark"
+    },
+    {
+      "Country": "NEPAL",
+      "val": 30174.17303,
+      "colr": "dark"
+    },
+    {
+      "Country": "PORTUGAL",
+      "val": 20045.44482,
+      "colr": "dark"
+    },
+    {
+      "Country": "TURKEMINSTAN",
+      "val": 22224.6896,
+      "colr": "dark"
+    },
+    {
+      "Country": "EL SLAVADOR",
+      "val": 9204.440643,
+      "colr": ""
+    },
+    {
+      "Country": "MOROCCO",
+      "val": 20941.88744,
+      "colr": "dark"
+    },
+    {
+      "Country": "SYRIA",
+      "val": 13851.81555,
+      "colr": "dark"
+    },
+    {
+      "Country": "INDIA",
+      "val": 11480.59502,
+      "colr": "dark"
+    },
+    {
+      "Country": "ALGERIA",
+      "val": 10572.66614,
+      "colr": "dark"
+    },
+    {
+      "Country": "AZERBAIJAN",
+      "val": 12926.70728,
+      "colr": "dark"
+    },
+    {
+      "Country": "JAPAN",
+      "val": 5004.972161,
+      "colr": ""
+    },
+    {
+      "Country": "PAKISTAN",
+      "val": 6844.213193,
+      "colr": ""
+    },
+    {
+      "Country": "BULGARIA",
+      "val": 4582.950195,
+      "colr": ""
+    },
+    {
+      "Country": "ARMENIA",
+      "val": 7961.370145,
+      "colr": ""
+    },
+    {
+      "Country": "GUINEA",
+      "val": 8624.180645,
+      "colr": ""
+    },
+    {
+      "Country": "ITALY",
+      "val": 4687.184245,
+      "colr": ""
+    },
+    {
+      "Country": "UZBEKISTAN",
+      "val": 6157.090792,
+      "colr": ""
+    },
+    {
+      "Country": "GUATEMALA",
+      "val": 3371.637416,
+      "colr": ""
+    },
+    {
+      "Country": "ROMANIA",
+      "val": 4612.826203,
+      "colr": ""
+    },
+    {
+      "Country": "CHILE",
+      "val": 2354.541979,
+      "colr": ""
+    },
+    {
+      "Country": "IRAN",
+      "val": 2921.609605,
+      "colr": ""
+    },
+    {
+      "Country": "INDONESIA",
+      "val": 851.809281,
+      "colr": "light"
+    },
+    {
+      "Country": "TURKEY",
+      "val": 2116.739723,
+      "colr": ""
+    },
+    {
+      "Country": "ECUADOR",
+      "val": 2391.391159,
+      "colr": ""
+    },
+    {
+      "Country": "YEMEN",
+      "val": 4548.925696,
+      "colr": ""
+    },
+    {
+      "Country": "TAIWAN",
+      "val": 1857.265683,
+      "colr": "light"
+    },
+    {
+      "Country": "EGYPT",
+      "val": 3963.712964,
+      "colr": ""
+    },
+    {
+      "Country": "KYRGYZSTAN",
+      "val": 1796.927688,
+      "colr": "light"
+    },
+    {
+      "Country": "PERU",
+      "val": 1160.821251,
+      "colr": "light"
+    },
+    {
+      "Country": "NEW ZEALAND",
+      "val": 780.4592925,
+      "colr": "light"
+    },
+    {
+      "Country": "SPAIN",
+      "val": 2408.606586,
+      "colr": ""
+    },
+    {
+      "Country": "TANZANIA",
+      "val": 2642.861195,
+      "colr": ""
+    },
+    {
+      "Country": "ISRAEL",
+      "val": 2301.178485,
+      "colr": ""
+    },
+    {
+      "Country": "NICARAGUA",
+      "val": 2191.308639,
+      "colr": ""
+    },
+    {
+      "Country": "MALAWI",
+      "val": 1194.239177,
+      "colr": "light"
+    },
+    {
+      "Country": "GREECE",
+      "val": 1397.135669,
+      "colr": "light"
+    },
+    {
+      "Country": "VENEZUELA",
+      "val": 1586.376252,
+      "colr": "light"
+    },
+    {
+      "Country": "GUADELOUPE",
+      "val": 1457.287013,
+      "colr": "light"
+    },
+    {
+      "Country": "LIBYA",
+      "val": 2055.777778,
+      "colr": ""
+    },
+    {
+      "Country": "TAJIKISTAN",
+      "val": 1446.619052,
+      "colr": "light"
+    },
+    {
+      "Country": "CROATIA",
+      "val": 1364.17191,
+      "colr": "light"
+    },
+    {
+      "Country": "AFGHANISTAN",
+      "val": 905.8101637,
+      "colr": "light"
+    },
+    {
+      "Country": "KAZAKHSTAN",
+      "val": 1107.454627,
+      "colr": "light"
+    },
+    {
+      "Country": "COLOMBIA",
+      "val": 955.2384451,
+      "colr": "light"
+    },
+    {
+      "Country": "ARGENTINA",
+      "val": 1358.061879,
+      "colr": "light"
+    },
+    {
+      "Country": "MACEDONIA",
+      "val": 1548.244392,
+      "colr": "light"
+    },
+    {
+      "Country": "IRAQ",
+      "val": 1141.309468,
+      "colr": "light"
+    },
+    {
+      "Country": "MEXICO",
+      "val": 496.8713256,
+      "colr": "light"
+    },
+    {
+      "Country": "PHILIPPINES",
+      "val": 500.234814,
+      "colr": "light"
+    },
+    {
+      "Country": "MYANMAR (BURMA)",
+      "val": 709.8818874,
+      "colr": "light"
+    },
+    {
+      "Country": "RWANDA",
+      "val": 1260.17527,
+      "colr": "light"
+    },
+    {
+      "Country": "SERBIA AND MONTENEGRO",
+      "val": 899.4303807,
+      "colr": "light"
+    },
+    {
+      "Country": "JAMAICA",
+      "val": 878.807856,
+      "colr": "light"
+    },
+    {
+      "Country": "ERITREA",
+      "val": 895.2774194,
+      "colr": "light"
+    },
+    {
+      "Country": "GHANA",
+      "val": 914.8120732,
+      "colr": "light"
+    },
+    {
+      "Country": "BELGIUM",
+      "val": 1074.4,
+      "colr": "light"
+    },
+    {
+      "Country": "JORDAN",
+      "val": 822.5509434,
+      "colr": "light"
+    },
+    {
+      "Country": "GEORGIA",
+      "val": 760.862741,
+      "colr": "light"
+    },
+    {
+      "Country": "CYPRUS",
+      "val": 798.4487405,
+      "colr": "light"
+    },
+    {
+      "Country": "UKRAINE",
+      "val": 449.6314523,
+      "colr": "light"
+    },
+    {
+      "Country": "CONGO",
+      "val": 732.5470235,
+      "colr": "light"
+    },
+    {
+      "Country": "DOMINICAN REPUBLIC",
+      "val": 334.2343945,
+      "colr": "light"
+    },
+    {
+      "Country": "BRAZIL",
+      "val": 861.7879599,
+      "colr": "light"
+    },
+    {
+      "Country": "HONDURAS",
+      "val": 470.7876713,
+      "colr": "light"
+    },
+    {
+      "Country": "BHUTAN",
+      "val": 276.0777496,
+      "colr": "light"
+    },
+    {
+      "Country": "USA",
+      "val": 176.097561,
+      "colr": "light"
+    },
+    {
+      "Country": "ALBANIA",
+      "val": 455.031184,
+      "colr": "light"
+    },
+    {
+      "Country": "MOZAMBIQUE",
+      "val": 446.3885714,
+      "colr": "light"
+    },
+    {
+      "Country": "ETHIOPIA",
+      "val": 487.6858909,
+      "colr": "light"
+    },
+    {
+      "Country": "UGANDA",
+      "val": 396.9907834,
+      "colr": "light"
+    },
+    {
+      "Country": "BOLIVIA",
+      "val": 352.6775499,
+      "colr": "light"
+    },
+    {
+      "Country": "RUSSIA",
+      "val": 203.2131973,
+      "colr": "light"
+    },
+    {
+      "Country": "COSTA RICA",
+      "val": 236.4515485,
+      "colr": "light"
+    },
+    {
+      "Country": "PANAMA",
+      "val": 248.9846314,
+      "colr": "light"
+    },
+    {
+      "Country": "BANGLADESH",
+      "val": 373.0010465,
+      "colr": "light"
+    },
+    {
+      "Country": "PAPUA NEW GUINEA",
+      "val": 205.9293226,
+      "colr": "light"
+    },
+    {
+      "Country": "CUBA",
+      "val": 269.6062924,
+      "colr": "light"
+    },
+    {
+      "Country": "FRANCE",
+      "val": 302.8306534,
+      "colr": "light"
+    },
+    {
+      "Country": "SOLOMON ISLANDS",
+      "val": 146.825766,
+      "colr": "light"
+    },
+    {
+      "Country": "USA TERRITORY",
+      "val": 212.8980152,
+      "colr": "light"
+    },
+    {
+      "Country": "AZORES (PORTUGAL)",
+      "val": 245.1371898,
+      "colr": "light"
+    },
+    {
+      "Country": "SLOVENIA",
+      "val": 262.1858034,
+      "colr": "light"
+    },
+    {
+      "Country": "SOUTH AFRICA",
+      "val": 245.2518818,
+      "colr": "light"
+    },
+    {
+      "Country": "WALLIS AND FUTUNA (FRENCH TERRITORY)",
+      "val": 208.6125,
+      "colr": "light"
+    },
+    {
+      "Country": "SOLOMON SEA",
+      "val": 179.7316998,
+      "colr": "light"
+    },
+    {
+      "Country": "NEW CALEDONIA",
+      "val": 165.1132776,
+      "colr": "light"
+    },
+    {
+      "Country": "VANUATU",
+      "val": 118.5746627,
+      "colr": "light"
+    },
+    {
+      "Country": "TUNISIA",
+      "val": 191.5665094,
+      "colr": "light"
+    },
+    {
+      "Country": "SAMAO",
+      "val": 132.5985606,
+      "colr": "light"
+    },
+    {
+      "Country": "SWITZERLAND",
+      "val": 178.9947025,
+      "colr": "light"
+    },
+    {
+      "Country": "MARTINIQUE",
+      "val": 124.1812328,
+      "colr": "light"
+    },
+    {
+      "Country": "SAUDI ARABIA",
+      "val": 123.1000422,
+      "colr": "light"
+    },
+    {
+      "Country": "SUDAN",
+      "val": 82.92590587,
+      "colr": "light"
+    },
+    {
+      "Country": "GERMANY",
+      "val": 188.4386492,
+      "colr": "light"
+    },
+    {
+      "Country": "MALAYSIA",
+      "val": 151.2101792,
+      "colr": "light"
+    },
+    {
+      "Country": "AUSTRALIA",
+      "val": 114.4773096,
+      "colr": "light"
+    },
+    {
+      "Country": "SOUTH KOREA",
+      "val": 141.4519805,
+      "colr": "light"
+    },
+    {
+      "Country": "FIJI",
+      "val": 97.56124205,
+      "colr": "light"
+    },
+    {
+      "Country": "MONGOLIA",
+      "val": 90.88250098,
+      "colr": "light"
+    },
+    {
+      "Country": "KERMADEC ISLANDS (NEW ZEALAND)",
+      "val": 89.39857391,
+      "colr": "light"
+    },
+    {
+      "Country": "SOUTH GEORGIA AND THE SOUTH SANDWICH ISLANDS",
+      "val": 65.07632825,
+      "colr": "light"
+    },
+    {
+      "Country": "HUNGARY",
+      "val": 116.4214682,
+      "colr": "light"
+    },
+    {
+      "Country": "NORTH KOREA",
+      "val": 134.5128914,
+      "colr": "light"
+    },
+    {
+      "Country": "ATLANTIC OCEAN",
+      "val": 118.325298,
+      "colr": "light"
+    },
+    {
+      "Country": "SLOVAKIA",
+      "val": 375.0545455,
+      "colr": "light"
+    },
+    {
+      "Country": "MICRONESIA, FE. STATES OF",
+      "val": 73.91948052,
+      "colr": "light"
+    },
+    {
+      "Country": "ZAMBIA",
+      "val": 113.030137,
+      "colr": "light"
+    },
+    {
+      "Country": "KIRIBATI",
+      "val": 108.5684211,
+      "colr": "light"
+    },
+    {
+      "Country": "BERING SEA",
+      "val": 123.1522388,
+      "colr": "light"
+    },
+    {
+      "Country": "GABON",
+      "val": 133.083871,
+      "colr": "light"
+    },
+    {
+      "Country": "INDIAN OCEAN",
+      "val": 104.9602191,
+      "colr": "light"
+    },
+    {
+      "Country": "CENTRAL AFRICAN REPUBLIC",
+      "val": 171.9,
+      "colr": "light"
+    },
+    {
+      "Country": "UK TERRITORY",
+      "val": 108.5684211,
+      "colr": "light"
+    },
+    {
+      "Country": "NORWAY",
+      "val": 142.262069,
+      "colr": "light"
+    },
+    {
+      "Country": "CAMEROON",
+      "val": 133.083871,
+      "colr": "light"
+    },
+    {
+      "Country": "VIETNAM",
+      "val": 183.36,
+      "colr": "light"
+    },
+    {
+      "Country": "FRENCH POLYNESIA",
+      "val": 126.9415385,
+      "colr": "light"
+    },
+    {
+      "Country": "SIERRA LEONE",
+      "val": 158.6769231,
+      "colr": "light"
+    },
+    {
+      "Country": "LAOS",
+      "val": 130.9714286,
+      "colr": "light"
+    },
+    {
+      "Country": "TOGO",
+      "val": 147.3428571,
+      "colr": "light"
+    },
+    {
+      "Country": "PALAU",
+      "val": 108.5684211,
+      "colr": "light"
+    },
+    {
+      "Country": "ANTARCTICA",
+      "val": 106.5589258,
+      "colr": "light"
+    },
+    {
+      "Country": "PACIFIC OCEAN",
+      "val": 122.6407869,
+      "colr": "light"
+    },
+    {
+      "Country": "COTE D'IVOIRE",
+      "val": 160.1576708,
+      "colr": "light"
+    },
+    {
+      "Country": "TONGA",
+      "val": 99.87422136,
+      "colr": "light"
+    },
+    {
+      "Country": "CANADA",
+      "val": 118.1712578,
+      "colr": "light"
+    },
+    {
+      "Country": "UK ",
+      "val": 155.2833335,
+      "colr": "light"
+    },
+    {
+      "Country": "NETHERLANDS",
+      "val": 147.8846154,
+      "colr": "light"
+    },
+    {
+      "Country": "LEBANON",
+      "val": 141.6430345,
+      "colr": "light"
+    },
+    {
+      "Country": "ICELAND",
+      "val": 99.75269007,
+      "colr": "light"
+    },
+    {
+      "Country": "ANTIGUA AND BARBUDA",
+      "val": 95.418,
+      "colr": "light"
+    },
+    {
+      "Country": "BOSNIA-HERZEGOVINA",
+      "val": 117.3059012,
+      "colr": "light"
+    },
+    {
+      "Country": "AUSTRIA",
+      "val": 113.8880383,
+      "colr": "light"
+    },
+    {
+      "Country": "THAILAND",
+      "val": 122.5164513,
+      "colr": "light"
+    },
+    {
+      "Country": "POLAND",
+      "val": 135.2333333,
+      "colr": "light"
+    },
+    {
+      "Country": "TRINIDAD AND TOBAGO",
+      "val": 82.98611808,
+      "colr": "light"
+    },
+    {
+      "Country": "BURUNDI",
+      "val": 126.8340426,
+      "colr": "light"
+    },
+    {
+      "Country": "CZECH REPUBLIC",
+      "val": 102.2243902,
+      "colr": "light"
+    },
+    {
+      "Country": "DJIBOUTI",
+      "val": 65.25714286,
+      "colr": "light"
+    },
+    {
+      "Country": "KENYA",
+      "val": 85.46086957,
+      "colr": "light"
+    }
+  ]
---
+++ assets/scripts/worldmap.js
@@ -0,0 +1,121 @@
+var map;
+      function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 2,
+          center: new google.maps.LatLng(2.8,-187.3),
+          mapTypeId: 'terrain'
+        });
+
+        // Create a <script> tag and set the USGS URL as the source.
+        var script = document.createElement('script');
+        script.src = 'assets/data/damage.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+      }//initMap
+
+
+        var labels=[]; 
+        var x=[];
+        var color=[];
+        var pinColors=[];
+         // Loop through the results array and place a marker for each  
+         var allR=colordata;
+      window.eqfeed_callback = function(results) {
+        var colr=[];
+        for (var i = 0; i < results.features.length; i++) {
+          var latLng = new google.maps.LatLng(results.features[i].LATITUDE, results.features[i].LONGITUDE);
+          labels[i]= {
+			  'country' : results.features[i].COUNTRY,
+			  'tsunami' : results.features[i].FLAG_TSUNAMI
+		  };
+      for (var j=0; j<allR.length; j++){
+      if(results.features[i].FLAG_TSUNAMI=='Tsu')
+      {
+         if(results.features[i].COUNTRY == allR[j].Country)
+          {
+            if(allR[j].colr=="dark")
+            pinColors[i]="0000A0"; 
+            else if(allR[j].colr=="light")
+              pinColors[i]="6666FF";
+            else
+              pinColors[i]="0000FF";
+          }        
+      }//if
+      else{
+
+        if(results.features[i].COUNTRY == allR[j].Country)
+          {
+            
+            if(allR[j].colr=="dark")
+            pinColors[i]="A00000"; 
+            else if(allR[j].colr=="light")
+              pinColors[i]="FF6666";
+            else
+              pinColors[i]="FF0000";
+
+          } 
+       }//else
+     }//inside for
+          var tmp={
+            'lat':results.features[i].LATITUDE,
+            'lng': results.features[i].LONGITUDE
+          };
+          x.push(tmp);
+         
+        }//for
+       
+      /* var colourname=[];
+      
+      for (var i=0; i<allR.length; i++) {
+        if((all[i].val > 60 ) && (all[i].val < 90850))
+          colourname[i]="light"+colr[i];
+        else if ((all[i].val > 90850 ) && (all[i].val < 181630))
+          colourname[i] = colr[i];
+        else
+          colourname[i] = "dark"+colr[i];
+      }//for
+
+      
+*/
+      var markers = x.map(function(location, i) {
+        var mark = new google.maps.Marker({
+            position: location,
+			index: i,
+            color: 'blue', 
+            icon: new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColors[i],
+              new google.maps.Size(21, 34),
+              new google.maps.Point(0,0),
+              new google.maps.Point(10, 34))
+
+          });
+		  var label = labels[mark.index].country;
+       var contentString = "<span> <b>Place:</b> " + label+ 
+	   "<br/> <b>Tsunami? </b> " + (labels[mark.index].tsunami=='Tsu'?'Yes':'No') + 
+	   "<br/> <b>Frequency: </b>"+ composite[label].frequency + 
+	   "<br/> <b>Avg. Magnitude: </b>" + composite[label].magnitude + 
+	   "<br/> <b>Avg. Damage:</b> $" +  composite[label].damage + " M</span>";
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+		mark.addListener('click', function() {
+
+        redrawPlot(label,false);
+          });
+       mark.addListener('mouseout', function() {
+
+        if (infowindow) {
+         infowindow.close();
+          }
+          });
+        mark.addListener('mouseover', function() {
+        infowindow.open(map, mark);
+        });
+
+return mark;
+        });
+        
+         
+      var markerCluster = new MarkerClusterer(map, markers,
+           {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
